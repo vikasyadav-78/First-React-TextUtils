ff8695fc7ea75d2b1b7de2203db84fe702681fb0
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-03-26 15:56:37 +0530

    Enter The Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar'
+import TextForm from './Components/TextForm'
 
 function App() {
   return (
     <>
       <Navbar title="Navbar"  about="About"/>
+      <div className="container my-3">
+      <TextForm  heading="Enter the text to analyze" />
+      </div>
     </>
   );
 }
