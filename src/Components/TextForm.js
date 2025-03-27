f25630f9890ad00bc3a05487b2e18deec1947bfe
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-03-27 11:18:52 +0530

    changes

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React , {useState} from 'react'
+
+
 
 export default function TextForm(props) {
+        const [text, setText] = useState('Enter text here');
+
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 {/* <label for="myBox" class="form-label">Example textarea/</label> */}
-                <textarea className="form-control" id="myBox" rows="10"></textarea>
+                <textarea className="form-control" value={text} id="myBox" rows="10"></textarea>
             </div>
+            <button className='btn btn-primary' onClick={handdleUpClick}>Convert To UpparCase</button>
         </div>
+
     )
 }
