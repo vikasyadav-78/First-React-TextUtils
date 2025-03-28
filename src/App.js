f8970b3d0c9ab2232848adc596e60cb629017eac
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-03-28 15:04:48 +0530

    about changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar'
 import TextForm from './Components/TextForm'
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar title="Navbar"  about="About"/>
       <div className="container my-3">
       <TextForm  heading="Enter the text to analyze" />
+      {/* <About /> */}
       </div>
     </>
   );
