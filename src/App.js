f3d9e3c8422d905d66c8df636fcfb034c8e0f866
yadavvikas787840@gmail.com <yadavvikas787840@gmail.comcls>
2025-04-04 12:05:53 +0530

    install react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 import Alert from './Components/Alert';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar'
 import TextForm from './Components/TextForm'
 import React, { useState } from 'react';
 
+import { Routes, Route, Router } from "react-router-dom";
+// import Home from "./Home";
+// import About from "./About";
+
+
+
+
+
 function App() {
+
   const [mode, setMode] = useState('light'); // weather is dark mode 
   const [alert, setalert] = useState(null)
 
-  const showAlert = (message , type) =>{
+  const showAlert = (message, type) => {
     setalert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setalert(null)
@@ -25,23 +34,31 @@ function App() {
     if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = '#042743'
-      showAlert("Dark mode is enableed" , "Success");
-      
+      showAlert("Dark mode is enableed", "Success");
+
     } else {
       setMode("light")
       document.body.style.backgroundColor = 'white'
-      showAlert("White mode is enableed" , "Success");
+      showAlert("White mode is enableed", "Success");
     }
   }
   return (
-    <>
-      <Navbar title="Navbar" about="About" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" showAlert={showAlert}  mode={mode}/>
-        {/* <About /> */}
-      </div>
-    </>
+
+      <Router>
+        <Navbar title="Navbar" about="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+            <Routes>
+              <About />
+              {/* <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} /> */}
+              {/* <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode} /> */}
+              <Route path="/" element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={mode} />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+      </Router>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +17,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
